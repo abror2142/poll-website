4196daf25a8c329a59bdebf24b562942e8ab714f
Abrorbek Ismoilov <abror2142@gmail.com>
2024-08-26 17:52:39 +0500

    Dockerizing full app with docker compose.

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/*
+    To make requests to the same backend url can be 
+    made easier by assigning baseURL to axios
+*/
+
 export default axios.create({
     baseURL: 'http://localhost:8000'
 })
