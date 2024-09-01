8719c10e8d20db58b8ff16bfea3ffb3983b2a49b
Abrorbek Ismoilov <abror2142@gmail.com>
2024-09-01 15:19:22 +0500

    Correcting routing bug with react-router-dom and webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,33 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
-    output: {
-        path:path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            }
-        ]
-    },
-    devServer: {
-        port: 3000,
-    },
-};
+  output: {
+    path: path.join(__dirname, "/dist"), // the bundle output path
+    filename: "bundle.js", // the name of the bundle
+    publicPath: '/',
 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html", // to import index.html file inside index.js
+    }),
+  ],
+  devServer: {
+    port: 3000, // you can change the port
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
